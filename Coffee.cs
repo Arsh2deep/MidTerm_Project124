e4c1774d387a67083798a1893bfb1867cadfead9
jemery <jemery@ML-RefVm-799127>
2023-05-04 15:59:47 -0700

    Add project files.

---
+++ Coffee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm_Project124
+{
+    public class Coffee : Product
+    {
+        public Coffee()
+        {
+            Name = "Coffee";
+            Price = 2.0;
+            Points = 1;
+        }
+
+        public override void Buy()
+        {
+            // Deduct the price from the customer's balance
+            // Add points to the customer's membership account
+        }
+    }
+
+    public class Tea : Product
+    {
+        public Tea()
+        {
+            Name = "Tea";
+            Price = 1.5;
+            Points = 1;
+        }
+
+        public override void Buy()
+        {
+            // Deduct the price from the customer's balance
+            // Add points to the customer's membership account
+        }
+    }
+    public class Pastry : Product
+    {
+        public Pastry()
+        {
+            Name = "Pastry";
+            Price = 2.5;
+            Points = 2;
+        }
+
+        public override void Buy()
+        {
+            // Deduct the price from the customer's balance
+            // Add points to the customer's membership account
+        }
+    }
+
+}
---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm_Project124
+{
+    public abstract class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Points { get; set; }
+        public int Quantity { get; set; }
+
+        public abstract void Buy();
+    }
+}
